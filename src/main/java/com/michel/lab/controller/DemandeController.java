17e4fccec393779b2c69fa427c8607d77959d906
Senpothia <kintelalpz@gmail.com>
2020-09-10 08:29:30 +0200

    Implémentation modification demande

--- src/main/java/com/michel/lab/controller/DemandeController.java
+++ src/main/java/com/michel/lab/controller/DemandeController.java
@@ -21,61 +21,86 @@ import com.michel.lab.service.jpa.UserService;
 @RestController
 @RequestMapping("/lab-service/private/demande")
 public class DemandeController {
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@Autowired
 	DemandeService demandeService;
-	
+
 	@PostMapping("/enregistrer")
 	public void enregistrerDemande(@RequestBody FormDemande formDemande) {
-		
+
 		Demande demande = new Demande();
 		demande.setNumero(formDemande.getNumero());
-	//	demande.setDate(formDemande.getDate());
+		// demande.setDate(formDemande.getDate());
 		demande.setEchantillon(formDemande.getEchantillon());
 		demande.setEssai(formDemande.getEssai());
 		demande.setObjectif(formDemande.getObjectif());
 		demande.setOrigine(formDemande.getOrigine());
 		demande.setProduit(formDemande.getProduit());
-		
+
 		Integer demandeur = formDemande.getDemandeur();
-		
+
 		Utilisateur utilisateur = userService.obtenirUser(demandeur);
 		demande.setDemandeur(utilisateur);
-		
+
 		demandeService.enregistrerDemande(demande);
-		
+
 	}
-	
+
 	@GetMapping("/liste")
-	public List<DemandeAux> listeDemandes(){
-		
+	public List<DemandeAux> listeDemandes() {
+
 		List<Demande> demandes = demandeService.obtenirListeDemandes();
 		List<DemandeAux> demandesAux = new ArrayList<DemandeAux>();
-		for(Demande d: demandes) {
-			
+		for (Demande d : demandes) {
+
 			DemandeAux demAux = new DemandeAux(d);
 			demandesAux.add(demAux);
 		}
 		return demandesAux;
 	}
-	
+
 	@GetMapping("/voir/{id}")
 	public DemandeAux voirDemande(@PathVariable(name = "id") Integer id) {
-		
+
 		Demande demande = demandeService.obtenirDemandeParId(id);
 		DemandeAux demAux = new DemandeAux(demande);
-		
+
 		return demAux;
-	
+
 	}
-	
+
 	@GetMapping("/supprimer/{id}")
 	public void supprimerDemande(@PathVariable(name = "id") Integer id) {
-		
+
 		demandeService.supprimerDemande(id);
+	}
+
+	@PostMapping("/modifier")
+	public void modifierDemande(@RequestBody FormDemande formDemande) {
+
+		Integer id = formDemande.getId();
+		System.out.println("id demande récupéré: " + id);
+		
+		Demande demande = demandeService.obtenirDemandeParId(id);
+
+		demande.setNumero(formDemande.getNumero());
+		// demande.setDate(formDemande.getDate());
+		demande.setEchantillon(formDemande.getEchantillon());
+		demande.setEssai(formDemande.getEssai());
+		demande.setObjectif(formDemande.getObjectif());
+		demande.setOrigine(formDemande.getOrigine());
+		demande.setProduit(formDemande.getProduit());
+
+		Integer demandeur = formDemande.getDemandeur();
+
+		Utilisateur utilisateur = userService.obtenirUser(demandeur);
+		demande.setDemandeur(utilisateur);
+
+		demandeService.enregistrerDemande(demande);
+
 	}
 
 }
