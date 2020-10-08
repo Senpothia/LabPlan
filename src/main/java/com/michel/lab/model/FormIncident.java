871a1c023d5a6e5a0ef06e09257e4bf8e7af7529
Senpothia <kintelalpz@gmail.com>
2020-10-08 20:37:39 +0200

    Ajout classe FormIncident

---
+++ src/main/java/com/michel/lab/model/FormIncident.java
@@ -0,0 +1,47 @@
+package com.michel.lab.model;
+
+public class FormIncident {
+	
+	private String date;
+	private String produit;
+	private String code;
+	private String description;
+	
+	public FormIncident() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public FormIncident(String date, String produit, String code, String description) {
+		super();
+		this.date = date;
+		this.produit = produit;
+		this.code = code;
+		this.description = description;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getProduit() {
+		return produit;
+	}
+	public void setProduit(String produit) {
+		this.produit = produit;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+
+}
