af6a385e74c05ae9b820ab1d35375ed1d9be1894
Senpothia <kintelalpz@gmail.com>
2020-10-19 18:18:06 +0200

    Correction url du controleur de site

--- src/main/java/com/michel/lab/controller/SiteController.java
+++ src/main/java/com/michel/lab/controller/SiteController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +24,7 @@ import com.michel.lab.service.jpa.SiteService;
 import com.michel.lab.service.jpa.UserService;
 
 @RestController
-@RequestMapping("/lab-service/private/activite/site")
+@RequestMapping("/private/activite/site")
 
 public class SiteController {
 	
